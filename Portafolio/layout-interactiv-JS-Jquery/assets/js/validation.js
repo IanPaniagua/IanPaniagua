14f53564d592eb7919ea3feaedfb7cc0bc7561b8
IanPaniagua <paniagua.ian.de@gmail.com>
2024-04-08 08:52:13 +0200

    correct validate form JS

--- Portafolio/layout-interactiv-JS-Jquery/assets/js/validation.js
+++ Portafolio/layout-interactiv-JS-Jquery/assets/js/validation.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
+    $('form').validate();
     $("form input[name='date']").datepicker();
-    
     $('#btnSubmit').on('click', function() {
         console.log($('form').valid());
     });
